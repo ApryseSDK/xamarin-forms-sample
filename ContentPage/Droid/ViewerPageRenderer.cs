af441ac759e2fe5c08a648ddcbcf5ca0cb2af890
sgong-pdftron <sgong@pdftron.com>
2020-09-09 14:41:14 -0700

    demo custom appearance

--- ContentPage/Droid/ViewerPageRenderer.cs
+++ ContentPage/Droid/ViewerPageRenderer.cs
@@ -17,6 +17,10 @@ using pdftron.PDF.Tools.Utils;
 using pdftron.PDF.Config;
 using Android.Content.Res;
 using AndroidX.Fragment.App;
+using Color = Android.Graphics.Color;
+using pdftron.PDF;
+using pdftron.PDF.Annots;
+using Page = Xamarin.Forms.Page;
 
 [assembly: ExportRenderer(typeof(ViewerPage), typeof(ViewerPageRenderer))]
 namespace CustomRenderer.Droid
@@ -66,7 +70,12 @@ namespace CustomRenderer.Droid
             mPdfViewCtrl = view.FindViewById<pdftron.PDF.PDFViewCtrl>(Resource.Id.pdfviewctrl);
             AppUtils.SetupPDFViewCtrl(mPdfViewCtrl, PDFViewCtrlConfig.GetDefaultConfig(this.Context));
 
-            var file = Utils.CopyResourceToLocal(this.Context, Resource.Raw.sample, "sample", ".pdf");
+            mPdfViewCtrl.DocumentLoad += (sender, e) =>
+            {
+                SetAnnotationCanvas(sender, e);
+            };
+
+            var file = Utils.CopyResourceToLocal(this.Context, Resource.Raw.sixth, "sample", ".pdf");
             mPdfDoc = mPdfViewCtrl.OpenPDFUri(Android.Net.Uri.FromFile(file), "");
 
             FragmentActivity fragmentActivity = null;
@@ -138,6 +147,87 @@ namespace CustomRenderer.Droid
             mPdfViewCtrl = null;
             mPdfDoc?.Close();
             mPdfDoc = null;
+        }
+
+        private void SetAnnotationCanvas(object sender, System.EventArgs e)
+        {
+            AddCanvas(1);
+            AddCanvas(2);
+        }
+
+        private void AddCanvas(int pageNumber)
+        {
+            var zoom = mPdfViewCtrl.GetZoomForViewMode(mPdfViewCtrl.PageViewMode);
+            var firstPage = mPdfDoc.GetPage(pageNumber);
+            var cropBox = firstPage.GetCropBox();
+
+            var canvasHeight = (int)(cropBox.Height());
+            var canvasWidth = (int)(cropBox.Width());
+
+            var layout = new NativeCustomRelativeLayout(mPdfViewCtrl.Context);
+            var color = new Android.Graphics.Color(0, 255, 00, 20);
+            layout.SetBackgroundColor(color);
+            layout.SetRect(mPdfViewCtrl, new pdftronprivate.PDF.Rect(0, 0, canvasWidth, canvasHeight), pageNumber);
+            mPdfViewCtrl.AddView(layout);
+
+            var myButton = new Android.Widget.Button(mPdfViewCtrl.Context);
+            myButton.Text = "BUTTON";
+            myButton.SetTextColor(Android.Graphics.Color.Pink);
+            myButton.Click += CreateTextAnnotation;
+
+            layout.AddView(myButton);
+        }
+
+        private void CreateTextAnnotation(object sender, System.EventArgs e)
+        {
+            mPdfViewCtrl.DocLock(true);
+
+            var editText = new EditText(mPdfViewCtrl.Context);
+            editText.Text = "this is a test this is a test this is a test this is a test ";
+            editText.TextSize = 12;
+            editText.SetSingleLine(false);
+            editText.ImeOptions = Android.Views.InputMethods.ImeAction.None;
+            editText.LayoutParameters = new ViewGroup.LayoutParams(500, 200);
+            editText.SetTextColor(Color.Red);
+
+            // layout view
+            editText.Measure(500, 200);
+            editText.Layout(0, 0, 500, 200);
+
+            var page = 1;
+
+            var color = Color.BlueViolet;
+            var rectBox = new pdftronprivate.PDF.Rect(0, 0, 500, 200);
+
+            var nativeDoc = TypeConvertHelper.ConvPDFDocToNative(mPdfDoc);
+
+            var textAnnot = pdftronprivate.PDF.Annots.FreeText.Create(nativeDoc, rectBox);
+
+            textAnnot.FontSize = 12;
+            textAnnot.SetColor(Utils.Color2ColorPt(color), 3);
+            textAnnot.Contents = editText.Text;
+
+            nativeDoc.GetPage(page).AnnotPushBack(textAnnot);
+
+            textAnnot.RefreshAppearance();
+
+            AnnotUtils.CreateCustomFreeTextAppearance(editText, mPdfViewCtrl,
+                    textAnnot, page,
+                    rectBox);
+
+            mPdfViewCtrl.Update(textAnnot, page);
+
+            mPdfViewCtrl.DocUnlock();
+        }
+
+        public static ColorPt GetColor(Color color)
+        {
+            var r = color.R / 256.0;
+            var g = color.G / 256.0;
+            var b = color.B / 256.0;
+
+            var colorPt = new ColorPt(r, g, b);
+            return colorPt;
         }
     }
 }
